92c8cdfd3a623c7802c85dbe51f29b5d82d2b0bc
Ddyadz <albertmik2002@gmail.com>
2024-12-02 02:52:28 +0300

    Update link

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000/api',
-  //xhttps://domik-tut.onrender.com/api
+  baseURL: "https://domik-tut.onrender.com/api",
+  //http://localhost:4000/api
 });
 
 instance.interceptors.request.use((config) => {
-  config.headers.Authorization = window.localStorage.getItem('token');
+  config.headers.Authorization = window.localStorage.getItem("token");
   return config;
 });
 
