1c9a2813233bd962408a8f5b0b8a24825d2d93b2
Альберт Микаелян <110053567+Ddyadz01@users.noreply.github.com>
2024-12-03 14:42:26 +0300

    Update useUser.js

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -1,12 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { GetMe } from '../services/user.service';
-import {useSelector} from "react-redux"
 
-const {user} = useSelector(state => state.user)
 export const useGetMe = () =>
   useQuery({
     queryKey: ['get me'],
     queryFn: GetMe,
     refetchOnWindowFocus: false,
-    enabled: !!user?.token
   });
