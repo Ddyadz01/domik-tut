8d139303a410440e98dff2f8e07240a754e46192
Альберт Микаелян <110053567+Ddyadz01@users.noreply.github.com>
2024-12-04 08:36:39 +0300

    Update NotFound.jsx

--- client/src/_pages/404/NotFound.jsx
+++ client/src/_pages/404/NotFound.jsx
@@ -17,7 +17,7 @@ export const NotFound = () => {
           navigate("/profile");
           setLoading(true);
         } else {
-          navigate("/home");
+          navigate("/register");
           setLoading(true);
         }
       }
